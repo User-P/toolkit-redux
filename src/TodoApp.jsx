50b0d5ebc41038eb239f46a0cde600772ba6c4e4
User-P <pether.1994@gmail.com>
2022-06-20 01:15:03 -0500

    RTK Query

---
+++ src/TodoApp.jsx
@@ -0,0 +1,22 @@
+import { useGetTodosQuery } from "./store/apis/todosApi";
+
+export const TodoApp = () => {
+	const { data: todos = [], isLoading, isFetching } = useGetTodosQuery();
+
+	return (
+		<>
+			<h1>TodoApp </h1>
+			<hr />
+			<h4>isLoading: {isLoading ? "True" : "False"}</h4>
+			<pre>...</pre>
+			<ul>
+				{todos.map((todo) => (
+					<li key={todo.id}>
+						<strong>{todo.completed ? "Done" : "Pending"}</strong> {todo.title}
+					</li>
+				))}
+			</ul>
+			<button>Next TODO</button>
+		</>
+	);
+};
